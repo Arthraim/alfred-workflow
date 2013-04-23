005b0005645baea2e4f75620c629364f86ca0e3a
Arthur Wang <arthraim@gmail.com>
2013-04-23 19:30:00 +0800

    changed icon

--- url_encoding/url.py
+++ url_encoding/url.py
@@ -8,15 +8,19 @@ import urllib
 
 if cmd == 'en':
     handled = urllib.quote(rawstring)
+    icon = '703B65EB-7EA7-40D9-8860-4BA3D2CF48A9.png'
 elif cmd == 'de':
     handled = urllib.unquote(rawstring)
+    icon = 'AA6E0037-6E59-499D-ABE1-8C5BC0BC48B4.png'
 else:
     handled = rawstring
+    icon = 'icon.png'
 
 results = [alfred.Item(
     attributes= {'uid': alfred.uid(0), 'arg': handled},
     title=handled,
-    subtitle=u'Enter to copy to clipboard'
+    subtitle=u'Enter to copy to clipboard',
+    icon=icon
 )]
 xml = alfred.xml(results)
 alfred.write(xml)
